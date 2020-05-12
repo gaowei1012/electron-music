57a5ddae4fe6c769fd94e772deda99d34cb4f3ef
苏小妍 <13666683140@163.com>
2020-05-12 23:56:13 +0800

    更新login页面

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+// const electron = require("electron");
+// const app = electron.app;
+// const BrowserWindow = electron.BrowserWindow;
+const {
+  app,
+  BrowserWindow,
+  // globalShortcut,
+  // dialog,
+  // Menu,
+  // ipcMain,
+} = require("electron");
 
 const path = require("path");
 const url = require("url");
@@ -12,10 +20,12 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 900,
     height: 680,
+    useContentSize: true,
+    frame: true, //添加这一行采用无边框窗口
   });
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
+      ? "http://localhost:8000"
       : `file://${path.join(__dirname, "./public/index.html")}`
   );
   mainWindow.on("closed", () => (mainWindow = null));
--- src/view/login/Login.jsx
+++ src/view/login/Login.jsx
@@ -1,25 +1,62 @@
 import React, { PureComponent } from "react";
+import { Input, Button } from "antd";
+import "./index.scss";
+// import { UserOutlined } from "@ant-design/icons";
+// const {
+//   app,
+//   BrowserWindow,
+//   globalShortcut,
+//   dialog,
+//   Menu,
+//   ipcMain,
+// } = require("electron");
+// const path = require("path");
 
+// let mainWindow;
+// const { dialog } = require("electron");
 class Login extends PureComponent {
-  state = {};
+  state = {
+    phone: "",
+    pwd: "",
+  };
   componentDidMount() {}
+  // _openDialog = () => {
+  //   dialog.showOpenDialog();
+  // };
 
-  handleBack() {
+  // 返回上一级
+  handleBack = () => {
     window.history.back(-1);
-    // this.history.goback
-  }
-
+  };
+  // 登录
+  handleLogin = () => {};
+  // 输入框信息
+  changeInput = (e) => {
+    console.log("111", e);
+  };
   _renderLogin() {
     return (
       <>
-        {/* history.goback */}
         <div onClick={this.handleBack}>返回</div>
-        <div>111</div>
+        <div className="login-box1">
+          <div className="box">
+            {/* onClick={() => this.handleItem(item.id, item.title)} */}
+            {/* <Input placeholder="请输入手机号" prefix={<UserOutlined />} /> */}
+            <Input placeholder="请输入手机号" onChange={this.changeInput} />
+            <Input.Password placeholder="请输入密码" />
+          </div>
+          <Button onClick={this.handleLogin}>登录</Button>
+        </div>
       </>
     );
   }
   render() {
-    return <>{this._renderLogin()}</>;
+    return (
+      <>
+        {this._renderLogin()}
+        {/* {this._openDialog()} */}
+      </>
+    );
   }
 }
 
